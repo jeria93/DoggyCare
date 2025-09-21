f1d48ff76de5352769cbf8fd145c25dede5b5541
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-09-21 22:27:12 +0200

    feat(ui): style Welcome
    - remove App.css
    - add welcome.css
    - center Welcome component

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Welcome.css";
+
 export default function Welcome() {
   return (
-    <main>
-      <h1>Doggy-Care</h1>
-      <p>Welcome to the Doggy Daycare</p>
+    <main className="welcome">
+      <h1 className="welcome__title">Doggy-Care</h1>
+      <p className="welcome__text">Welcome to the Doggy Daycare</p>
+      <Link className="welcome__btn" to="/dogs">
+        View Dogs
+      </Link>
     </main>
   );
 }
